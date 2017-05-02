1c981dbf2317cd5488f0fa83f98f282b82905c2c
Danny Dang <dnd003@ucsd.edu>
2017-05-02 01:59:24 -0700

    t

--- GhostTag/index.js
+++ GhostTag/index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', '/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 
